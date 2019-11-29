e1cebf7ae454b83c6911ca6d5c29a627a81eecfd
ck2e14 <chriswkennedy@icloud.com>
2019-11-29 08:05:30 +0000

    updated login params, login method, and validate method

--- shittyMap/app/controllers/api/v1/users_controller.rb
+++ shittyMap/app/controllers/api/v1/users_controller.rb
@@ -8,13 +8,39 @@ class Api::V1::UsersController < ApplicationController
      end
    end
 
-   def index 
-      @users = User.all
-      render json: @users
-   end
-  
-   private
-   def user_params
-     params.require(:user).permit(:username, :password)
-   end
- end
+  def login
+    user = User.find_by(username: login_params[:username])
+    if user && user.authenticate(login_params[:password])
+      render json: { user: UserSerializer.new(user), token: issue_token({ user_id: user.id }) }
+    else
+      render json: { errors: ["Email or password incorrect"] }, status: :not_accepted
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def index 
+    @users = User.all
+    render json: @users
+  end
+
+  def validate
+    if logged_in
+        render json: { user: UserSerializer.new(@current_user), token: issue_token({ user_id: @current_user.id }) }
+    else
+        render json: { errors: ['Invalid token']}, status: :not_accepted
+    end
+  end
+    
+    private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def login_params
+    params.require(:user).permit(:username, :password)
+  end
+end
