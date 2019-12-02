0e3c94fa764536912d23109a5d6edd188d02346a
ck2e14 <chriswkennedy@icloud.com>
2019-12-02 17:47:34 +0000

    fixed conditional logout button not displaying when creating a user and being redirected to newJourney

--- shittyMap-backend/app/controllers/api/v1/users_controller.rb
+++ shittyMap-backend/app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created
+      render json: { user: UserSerializer.new(@user), token: issue_token(user_id: @user.id) }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
