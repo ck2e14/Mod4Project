3a37320ec18a5968f60324d8101b56ef823fdaba
Luke Prendergast <luke.prendergast94@gmail.com>
2019-12-04 13:44:42 +0000

    version control

--- shittyMap-frontend/src/pages/Journeys/MapContainer.js
+++ shittyMap-frontend/src/pages/Journeys/MapContainer.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-
-
-export class MapContainer extends Component {
-  render() {
-    return (
-      <div>
-        <Map google={this.props.google}
-          />
-      </div>
-    )
-  }
-}
-
-
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyA4KsBwrhZbB_bnjtmZi1x67YTMMWmtQuU'
-})(MapContainer);
+// import React, { Component } from 'react';
+// import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+//
+//
+// export default class MapContainer extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Map google={this.props.google}
+//
+//           />
+//       </div>
+//     )
+//   }
+// }
