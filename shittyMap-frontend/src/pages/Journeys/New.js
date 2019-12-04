ff7037a22fa780d0cebb79c425bc952e4a8900a1
ck2e14 <chriswkennedy@icloud.com>
2019-12-04 10:06:04 +0000

    fixed un-enclosed JSX </div>

--- shittyMap-frontend/src/pages/Journeys/New.js
+++ shittyMap-frontend/src/pages/Journeys/New.js
@@ -77,70 +77,8 @@ export default class NewJourney extends Component {
           {this.props.user ? <button onClick={this.props.logout}>Log Out</button> : null}
         </div>
       </div>
-
+ </div>
   );
 }
 
 };
-
-
-
-  // handleSubmit = (e, origin, destination) => {
-  //   const options = []
-  //   const proxyurl = "https://cors-anywhere.herokuapp.com/";
-  //   e.preventDefault()
-  //   fetch(`${proxyurl}https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=AIzaSyC46mxowyyPzXCDudxz8BO2YiTJkKs9M9I`)
-  //     .then(res => res.json())
-  //     .then(thing => options.push(thing))
-
-  //   console.log(options)
-
-  // };
-
-
-// onFormSubmit={this.onTermSubmit}
-
-
-// import React, { useState } from "react";
-// import API from "../../adapters/API";
-// import Map from './Map.js'
-// import SearchBar from './SearchBar.js'
-// import JourneyInformation from './JourneyInformation.js'
-// import ExtraInformation from './ExtraInformation'
-//
-//
-// import { useHistory } from "react-router-dom";
-//
-// const NewJourney = props => {
-//
-//   const [title, setStartpoint] = useState("");
-//   const [content, setEndpoint] = useState("");
-//   const history = useHistory();
-//
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     API.postJourney({ title, content }).then(journey => {
-//       history.push(`/journey/${journey.id}`);
-//     });
-//     setStartpoint("");
-//     setEndpoint("");
-//   };
-//   return (
-//     <div className='ui container'>
-//           <SearchBar />
-//             <div className='ui grid'>
-//               <div className='ui row'>
-//               <div className="eleven wide column">
-//                 <Map />
-//                 </div>
-//                 <div className="five wide column">
-//                 <JourneyInformation />
-//                 </div>
-//                 <ExtraInformation />
-//                 </div>
-//               </div>
-//         </div>
-//   );
-// };
-//
-// export default NewJourney;
