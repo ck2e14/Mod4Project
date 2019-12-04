fc46d020e8fdab539ede5ddcc2c94f8817e7113a
github username <github email address>
2019-12-04 10:29:50 +0000

    sanitised results from fetch

--- shittyMap-frontend/src/pages/Journeys/JourneyInformation.js
+++ shittyMap-frontend/src/pages/Journeys/JourneyInformation.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 
 
 export default class JourneyInformation extends Component {
+
+  getRoutes = () => {
+    const allRoutes = this.props.routes
+    if (allRoutes.length >= 8) {
+      {console.log(allRoutes)}
+    }
+  }
+
+
+
   render() {
     return (
       <div>
-          {console.log(this.props.routes)}
-        </div>
+          {this.getRoutes()}
+      </div>
     );
   }
 }
