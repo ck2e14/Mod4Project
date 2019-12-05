1095e47f18c25b77340ca724b3cc7870635ae568
ck2e14 <chriswkennedy@icloud.com>
2019-12-05 13:41:49 +0000

    minor fixes

--- shittyMap-frontend/src/pages/Dashboard/JourneyDisplay.js
+++ shittyMap-frontend/src/pages/Dashboard/JourneyDisplay.js
@@ -5,7 +5,11 @@ import API from "../../adapters/API";
 const JourneyDisplay = (props) => {
    return(
       <div>
-         <p>{props.journey.startpoint} </p>
+         <h5>---------------------</h5>
+         <p><strong>Origin: </strong>{props.journey.startpoint} </p>
+         <p><strong>Destination: </strong>{props.journey.endpoint}</p>
+         <p><strong>Duration: </strong>{props.journey.duration}</p>
+         <p><strong>Transport Type: </strong>{props.journey.transit_mode   }</p>
       </div>
    )
 }
